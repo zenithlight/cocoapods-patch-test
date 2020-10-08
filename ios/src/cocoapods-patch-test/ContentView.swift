8bc40b5a80403a24acedb118c732099c94da1054
Alexis Anand <alexis@neuralink.com>
2020-10-08 14:09:57 -0700

    Initial commit

---
+++ ios/src/cocoapods-patch-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cocoapods-patch-test
+//
+//  Created by Alexis Anand on 10/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/src/cocoapods-patch-test/cocoapods_patch_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocoapods_patch_testApp.swift
+//  cocoapods-patch-test
+//
+//  Created by Alexis Anand on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct cocoapods_patch_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
